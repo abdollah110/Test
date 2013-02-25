9c09b01cdb8eea32c9e0726438344af9e73c9561
abdollah110 <smohamadi@gmail.com>
2013-02-25 14:15:31 +0100

    Helper.help

--- final_Results.cc
+++ final_Results.cc
@@ -1,3 +1,3 @@
-dbs search --query="find dataset where dataset=/JetMETTau/Run2010A-Jul15thReReco-v1/RECO " | sort -n
+dbs search --query="find dataset where dataset=/JetMETTau/Run2010A-Jul15thReReco-v1/RECO "  | sort -n
 
 
